6a718b5c001dd909e56f7b85b3f80b4d3f900add
Kosta <93149642+Spitelon@users.noreply.github.com>
2023-12-06 01:53:56 +0100

    Add files via upload

--- script/script.js
+++ script/script.js
@@ -3,14 +3,12 @@ var countDownDate = new Date("Jun 5, 2024 15:37:25").getTime();
 var x = setInterval(function () {
   var now = new Date().getTime();
 
-
   var distance = countDownDate - now;
 
   var day = Math.floor(distance / (1000 * 60 * 60 * 24));
   var hour = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
   var minute = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
   var second = Math.floor((distance % (1000 * 60)) / 1000);
-
 
   document.getElementById("day").innerHTML = day + " ";
 
